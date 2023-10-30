fdc44da1d59b03355dbb449e830fc08e230c07f8
Ibon Azkoitia <ibon@kreatidos.com>
2023-10-30 19:01:07 +0100

    Todo preparado

---
+++ README.md
@@ -0,0 +1,4 @@
+# Curso | Tailwind CSS » Ejemplo - udemy.com
+
+En este repositorio puedes encontrar el código que utilizamos en
+el [Curso de Tailwind CSS](https://ibon.link/cursoTailwindCSS)
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,php}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': {
+          'light': '#3e4143',
+          DEFAULT: '#2d2e31'
+        },
+        'udemy': {
+          'light': '#a436f0',
+          DEFAULT: '#5624d0',
+        },
+        'accent': '#eceb98',
+      },
+      maxWidth: {
+        '8xl': '84rem',
+      }
+    },
+    fontFamily: {
+      'heading': ['Noto Serif JP', 'serif'],
+      'text': ['Roboto', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
+
