8c8cb304ba334f71a84489db729495d4ebfbed5f
Ibon Azkoitia <ibon@kreatidos.com>
2023-10-31 13:10:49 +0100

    Crear la estructura del Header

---
+++ components/header.php
@@ -0,0 +1,51 @@
+<header class="py-3 px-6 flex items-center flex-wrap gap-6 text-sm">
+    <img src="/assets/images/logo-udemy.svg" alt="Logo de Udemy" class="h-8">
+
+    <div class="hover:text-udemy">
+        Categorias
+    </div>
+
+    <div class="grow p-3 flex items-center gap-4 border border-primary rounded-full bg-gray-50 text-gray-500">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+        </svg>
+
+        <div>
+            Buscar cualquier cosa
+        </div>
+    </div>
+
+    <div class="hover:text-udemy">
+        Udemy Business
+    </div>
+
+    <div class="hover:text-udemy">
+        Instructor
+    </div>
+
+    <div class="hover:text-udemy">
+        Mi aprendizaje
+    </div>
+
+    <div>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6 hover:text-udemy">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+        </svg>
+    </div>
+
+    <div>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6 hover:text-udemy">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+        </svg>
+    </div>
+
+    <div>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6 hover:text-udemy">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+        </svg>
+    </div>
+
+    <div>
+        <img src="/assets/images/ibon-azkoitia.jpg" alt="Usuario" class="h-8 rounded-full border border-gray-300">
+    </div>
+</header>
--- index.php
+++ index.php
@@ -12,9 +12,10 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200;300;400;500;600;700;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="assets/style-production.css" />
+    <link rel="stylesheet" href="assets/css/style-production.css" />
 </head>
 <body>
 
+<?php require 'components/header.php'; ?>
 </body>
 </html>
