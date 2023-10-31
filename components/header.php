67540ea0d19943e053904e51a09b0d5a84c869fc
Ibon Azkoitia <ibon@kreatidos.com>
2023-10-31 15:03:26 +0100

    Crear el menu Udemy Business

--- components/header.php
+++ components/header.php
@@ -1,8 +1,166 @@
 <header class="py-3 px-6 flex items-center flex-wrap gap-6 text-sm">
     <img src="/assets/images/logo-udemy.svg" alt="Logo de Udemy" class="h-8">
 
-    <div class="hover:text-udemy">
-        Categorias
+    <div x-data="{
+            menuCategorias: false,
+            menuCategoriasDesarrollo: false,
+            menuCategoriasNegocios: false,
+            menuCategoriasFinanzas: false,
+            menuCategoriasDesarrolloDesarrolloWeb: false,
+            menuCategoriasDesarrolloCiencias: false
+        }">
+        <div class="hover:text-udemy hover:cursor-pointer" @click="menuCategorias = ! menuCategorias">Categorias</div>
+
+
+        <div x-show="menuCategorias" class="absolute mt-8 grid grid-cols-3 min-h-[300px]">
+            <div x-show="menuCategorias" class="w-[240px] border border-gray-300">
+                <ul>
+                    <li @click="menuCategoriasDesarrollo = ! menuCategoriasDesarrollo" @click.away="menuCategoriasDesarrollo = false"
+                        class="p-4 flex items-center justify-between hover:cursor-pointer hover:text-udemy"
+                    >
+                        Desarrollo
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-3 h-3">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
+                    </li>
+                    <li @click="menuCategoriasNegocios = ! menuCategoriasNegocios" @click.away="menuCategoriasNegocios = false"
+                        class="px-4 py-1 flex items-center justify-between hover:cursor-pointer hover:text-udemy"
+                    >
+                        Negocios
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-3 h-3">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
+                    </li>
+                    <li @click="menuCategoriasFinanzas = ! menuCategoriasFinanzas" @click.away="menuCategoriasFinanzas = false"
+                        class="p-4 flex items-center justify-between hover:cursor-pointer hover:text-udemy"
+                    >
+                        Finanzas y Contabilidad
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-3 h-3">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
+                    </li>
+
+                    <li class="px-4 py-1 flex items-center justify-between hover:cursor-pointer hover:text-udemy">
+                        Informática y software
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-3 h-3">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
+                    </li>
+
+                    <li class="p-4 flex items-center justify-between hover:cursor-pointer hover:text-udemy">
+                        Productividad en la oficina
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-3 h-3">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
+                    </li>
+
+                    <li class="px-4 py-1 flex items-center justify-between hover:cursor-pointer hover:text-udemy">
+                        Desarrollo personal
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-3 h-3">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
+                    </li>
+
+                    <li class="p-4 flex items-center justify-between hover:cursor-pointer hover:text-udemy">
+                        Diseño
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-3 h-3">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
+                    </li>
+
+                    <li class="px-4 py-1 flex items-center justify-between hover:cursor-pointer hover:text-udemy">
+                        Marketing
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-3 h-3">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
+                    </li>
+
+                    <li class="p-4 flex items-center justify-between hover:cursor-pointer hover:text-udemy">
+                        Estilo de vida
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-3 h-3">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
+                    </li>
+
+                    <li class="px-4 py-1 flex items-center justify-between hover:cursor-pointer hover:text-udemy">
+                        Fotografía y vídeo
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-3 h-3">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
+                    </li>
+
+                    <li class="p-4 flex items-center justify-between hover:cursor-pointer hover:text-udemy">
+                        Salud y Fitness
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-3 h-3">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
+                    </li>
+
+                    <li class="px-4 py-1 flex items-center justify-between hover:cursor-pointer hover:text-udemy">
+                        Música
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-3 h-3">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
+                    </li>
+
+                    <li class="p-4 flex items-center justify-between hover:cursor-pointer hover:text-udemy">
+                        Enseñazas y disciplinas académicas
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-3 h-3">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
+                    </li>
+                </ul>
+            </div>
+
+            <div x-show="menuCategoriasDesarrollo" class="border border-l-0 border-gray-300">
+                <ul>
+                    <li @mouseover="menuCategoriasDesarrolloDesarrolloWeb = true"
+                        @mouseout="menuCategoriasDesarrolloDesarrolloWeb = false"
+                        class="p-4 hover:cursor-pointer hover:text-udemy"
+                    >Desarrollo web</li>
+                    <li @mouseover="menuCategoriasDesarrolloCiencias = true"
+                        @mouseout="menuCategoriasDesarrolloCiencias = false"
+                        class="px-4 py-1 hover:cursor-pointer hover:text-udemy"
+                    >Ciencias de la información</li>
+                    <li class="p-4 hover:cursor-pointer hover:text-udemy">Desarrollo móvil</li>
+                </ul>
+            </div>
+
+            <div x-show="menuCategoriasNegocios" class="border border-l-0 border-gray-300">
+                <ul>
+                    <li class="p-4 hover:cursor-pointer hover:text-udemy">Comunicación</li>
+                    <li class="px-4 py-1 hover:cursor-pointer hover:text-udemy">Emprendimiento</li>
+                    <li class="p-4 hover:cursor-pointer hover:text-udemy">Gestión Empresarial</li>
+                </ul>
+            </div>
+
+            <div x-show="menuCategoriasFinanzas" class="p-4">
+                <ul class="space-y-4">
+                    <li>Contabilidad</li>
+                    <li>Cumplimiento</li>
+                    <li>Criptomonedas</li>
+                </ul>
+            </div>
+
+            <div x-show="menuCategoriasDesarrolloDesarrolloWeb" class="p-4 border border-l-0 border-gray-300">
+                <ul class="space-y-4">
+                    <li class="font-medium">Temas populares</li>
+                    <li>JavaScript</li>
+                    <li>React JS</li>
+                    <li>Angular</li>
+                </ul>
+            </div>
+
+            <div x-show="menuCategoriasDesarrolloCiencias" class="p-4 border border-l-0 border-gray-300">
+                <ul class="space-y-4">
+                    <li class="font-medium">Temas populares</li>
+                    <li>Python</li>
+                    <li>Aprendizaje automático</li>
+                    <li>Aprendizaje profundo</li>
+                </ul>
+            </div>
+        </div>
+
     </div>
 
     <div class="grow p-3 flex items-center gap-4 border border-primary rounded-full bg-gray-50 text-gray-500">
@@ -15,8 +173,14 @@
         </div>
     </div>
 
-    <div class="hover:text-udemy">
-        Udemy Business
+    <div x-data="{ menuUdemyBusiness: false }" class="relative" @click.away="menuUdemyBusiness = false">
+        <div @mouseover="menuUdemyBusiness = true" @click="menuUdemyBusiness = ! menuUdemyBusiness" class="hover:text-udemy hover:cursor-pointer">Udemy Business</div>
+
+        <div x-show="menuUdemyBusiness" class="absolute mt-8 right-0 w-[300px] border border-gray-300 p-4 text-center">
+            <div class="text-xl font-semibold">Dale a tu equipo acceso a más de 24.000 de los mejores cursos de Udemy en cualquier momento y lugar.</div>
+
+            <a href="#" class="block mt-4 p-4 bg-primary hover:bg-primary-light text-white text-lg">Prueba Udemy Business</a>
+        </div>
     </div>
 
     <div class="hover:text-udemy">
