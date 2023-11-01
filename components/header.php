91f67d9cb0d09ac5f86d859e4f38e5651fb8f895
Ibon Azkoitia <ibon@kreatidos.com>
2023-11-01 11:17:44 +0100

    Crear el menu 'Aprendizaje'

--- components/header.php
+++ components/header.php
@@ -183,12 +183,83 @@
         </div>
     </div>
 
-    <div class="hover:text-udemy">
-        Instructor
+    <div>
+        <a href="#" class="hover:text-udemy">Instructor</a>
     </div>
 
-    <div class="hover:text-udemy">
-        Mi aprendizaje
+    <div x-data="{ menuMiAprendizaje: false }" class="relative" @click.away="menuMiAprendizaje = false">
+        <div @mouseover="menuMiAprendizaje = true" @click="menuMiAprendizaje = ! menuMiAprendizaje" class="hover:text-udemy hover:cursor-pointer">
+            Mi aprendizaje
+        </div>
+
+        <div x-show="menuMiAprendizaje" class="absolute mt-8 right-0 w-[300px] border border-gray-300 divide-y divide-gray-300">
+
+            <div>
+                <a href="#" class="flex gap-3 p-4">
+                    <img src="/assets/images/header/aprendizaje/curso1.jpg" alt="" class="h-16 w-16 object-cover">
+                    <div>
+                        <p class="font-semibold leading-tight">
+                            Basics of Fibonacci: Price Action Trading Tool
+                        </p>
+
+                        <div class="mt-2 h-2 bg-gray-300">
+                            <div class="h-2 bg-udemy w-[10%]"></div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div>
+                <a href="#" class="flex gap-3 p-4">
+                    <img src="/assets/images/header/aprendizaje/curso2.jpg" alt="" class="h-16 w-16 object-cover">
+                    <div>
+                        <p class="font-semibold leading-tight">
+                            Day Trading & Swing Trading: Technical Analysis Basics 2023
+                        </p>
+
+                        <div class="mt-2 h-2 bg-gray-300">
+                            <div class="h-2 bg-udemy"></div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div>
+                <a href="#" class="flex gap-3 p-4">
+                    <img src="/assets/images/header/aprendizaje/curso3.jpg" alt="" class="h-16 w-16 object-cover">
+                    <div>
+                        <p class="font-semibold leading-tight">
+                            The Essential Guide to Airtable
+                        </p>
+
+                        <div class="mt-2 h-2 bg-gray-300">
+                            <div class="h-2 bg-udemy w-[5%]"></div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div>
+                <a href="#" class="flex gap-3 p-4">
+                    <img src="/assets/images/header/aprendizaje/curso4.jpg" alt="" class="h-16 w-16 object-cover">
+                    <div>
+                        <p class="font-semibold leading-tight">
+                            The Complete Ruby on Rails Developer Course
+                        </p>
+
+                        <div class="mt-2 h-2 bg-gray-300">
+                            <div class="h-2 bg-udemy w-1/4"></div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="p-4 text-center">
+                <a href="#" class="block p-4 bg-primary hover:bg-primary-light text-white text-lg">
+                    Ir a Mi Aprendizaje
+                </a>
+            </div>
+        </div>
     </div>
 
     <div>
