755ec41e2b9502dbec43423041cbb1113ff4a835
Ibon Azkoitia <ibon@kreatidos.com>
2023-11-13 13:48:40 +0100

    Actualizar la clase de Cursos

--- components/home/cursos.php
+++ components/home/cursos.php
@@ -25,38 +25,104 @@
 			</a>
 
 			<div class="mt-8 grid grid-cols-5 gap-4">
-				<div>
-					<img class="mb-4 border border-gray-300" src="https://img-b.udemycdn.com/course/240x135/2297910_788e_2.jpg" alt="">
-					<h4 class="line-clamp-2 font-semibold font-text leading-tight">Python Practicando. Desde 0 hasta Desarrollador en Python</h4>
-					<p class="my-1 text-xs text-gray-500">
-						Alvaro Chirou, Walter Coto
-					</p>
-					<div class="flex items-center gap-1">
+				<div class="relative" x-data="{toolTip: false }">
+					<div class="relative z-10" x-on:click="toolTip = ! toolTip"  x-on:mouseover="toolTip = true">
+						<img class="mb-4 border border-gray-300" src="https://img-b.udemycdn.com/course/240x135/2297910_788e_2.jpg" alt="">
+						<h4 class="line-clamp-2 font-semibold font-text leading-tight">Python Practicando. Desde 0 hasta Desarrollador en Python</h4>
+						<p class="my-1 text-xs text-gray-500">
+							Alvaro Chirou, Walter Coto
+						</p>
 						<div class="flex items-center gap-1">
-							<div class="font-semibold">4,6</div>
-							<div class="flex items-center">
-								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-[#b4690e] fill-[#b4690e]">
-									<path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-								</svg>
-								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-[#b4690e] fill-[#b4690e]">
-									<path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-								</svg>
-								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-[#b4690e] fill-[#b4690e]">
-									<path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-								</svg>
-								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-[#b4690e] fill-[#b4690e]">
-									<path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-								</svg>
-								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-[#b4690e] fill-[#b4690e]">
-									<path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-								</svg>
+							<div class="flex items-center gap-1">
+								<div class="font-semibold">4,6</div>
+								<div class="flex items-center">
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-[#b4690e] fill-[#b4690e]">
+										<path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+									</svg>
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-[#b4690e] fill-[#b4690e]">
+										<path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+									</svg>
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-[#b4690e] fill-[#b4690e]">
+										<path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+									</svg>
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-[#b4690e] fill-[#b4690e]">
+										<path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+									</svg>
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-[#b4690e] fill-[#b4690e]">
+										<path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+									</svg>
+								</div>
+							</div>
+							<div class="text-xs text-gray-500">
+								(20.414)
 							</div>
 						</div>
-						<div class="text-xs text-gray-500">
-							(20.414)
+						<p class="font-semibold">84,99 €</p>
+					</div>
+
+					<div x-show="toolTip" @click.outside="toolTip = false" class="z-20 absolute -right-[365px] -top-[65px] p-6 max-w-[350px] border border-gray-300 bg-white text-sm cursos-tooltip">
+						<a href="#" class="text-xl font-semibold leading-tight hover:text-udemy-dark">
+							Python Practicando. Desde 0 hasta Desarrollador en Python
+						</a>
+
+						<div class="my-2 text-[#1e6055] text-xs">
+							Actualizado <span class="font-semibold">febrero de 2023</span>
+						</div>
+
+						<div class="flex items-center text-xs text-gray-500 gap-2">
+							<div>25,5 horas en total</div>
+							<div>·</div>
+							<div>Nivel avanzado</div>
+							<div>·</div>
+							<div>Subtítulos</div>
+						</div>
+
+						<div class="my-3">
+							Aprende Python, donde iniciamos desde 0, sin conocimientos previos hasta desarrollar aplicaciones con mucha practica!
+						</div>
+
+						<div class="space-y-4">
+							<div class="flex gap-2">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="min-w-[30px] w-5 h-4">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+								</svg>
+
+								<div>
+									Ejercitar la lógica de programación
+								</div>
+							</div>
+							<div class="flex gap-2">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="min-w-[30px] w-5 h-4">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+								</svg>
+
+								<div>
+									Comprender cómo la vida cotidiana puede ser fácilitada o simulada con código
+								</div>
+							</div>
+							<div class="flex gap-2">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class=" min-w-[30px] w-5 h-4">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+								</svg>
+
+								<div>
+									Aprender programación desde cero
+								</div>
+							</div>
+						</div>
+
+						<div class="mt-4 flex justify-between items-center gap-2">
+							<a href="#" class="w-full py-2.5 px-6 bg-udemy-light hover:bg-[#870fd8] font-semibold text-white text-center text-base">
+								Añadir a la cesta
+							</a>
+							<div class="p-2 border border-gray-900 rounded-full hover:bg-gray-200 hover:cursor-pointer">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+								</svg>
+
+							</div>
 						</div>
 					</div>
-					<p class="font-semibold">84,99 €</p>
 				</div>
 			</div>
 		</div>
