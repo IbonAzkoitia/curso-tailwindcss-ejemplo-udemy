e5111ef1df18d376e1310bb9f3b6196f221731ec
Ibon Azkoitia <ibon@kreatidos.com>
2023-11-15 20:26:47 +0100

    Crear la clase 'Migas'

---
+++ components/curso-gratuito/migas.php
@@ -0,0 +1,21 @@
+<div class="shadow-lg -mt-3 h-3"></div>
+
+<div class="p-6">
+    <div class="flex items-center gap-2 text-udemy font-semibold text-sm">
+        <a href="#" class="hover:text-udemy-dark">
+            Desarrollo
+        </a>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-3 h-3">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+        </svg>
+        <a href="#" class="hover:text-udemy-dark">
+            Lenguaje de Programación
+        </a>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-3 h-3">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+        </svg>
+        <a href="#" class="hover:text-udemy-dark">
+            Python
+        </a>
+    </div>
+</div>
