118543f0f4086001514b46b9e972f460f6b80719
Ibon Azkoitia <ibon@kreatidos.com>
2023-12-09 15:04:30 +0100

    Crear clase 'Sidebar Estructura'

--- components/curso-pago/cabecera.php
+++ components/curso-pago/cabecera.php
@@ -1,5 +1,5 @@
-<div class="bg-[#2d2f31]">
-    <div class="max-w-6xl mx-auto pt-8 pb-10 text-white ">
+<div class="bg-[#2d2f31] ">
+    <div class="relative max-w-6xl mx-auto pt-8 pb-10 text-white ">
         <div class="max-w-2xl">
             <!-- Migas de Pan -->
             <div class="">
@@ -95,5 +95,153 @@
             </div>
         </div>
 
+        <div class="absolute top-8 right-0 w-[340px] border border-gray-300 bg-white shadow-sm text-primary">
+
+            <div class="h-[180px]">
+                <button class="relative w-full h-full flex flex-col items-center justify-center bg-cover bg-[url('https://img-c.udemycdn.com/course/240x135/4578076_28d7_6.jpg')]">
+                    <span class="absolute bg-gradient-to-t from-primary h-full w-full">
+
+                    </span>
+
+                    <span class="z-20">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-20 h-20 text-white">
+                          <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z" clip-rule="evenodd" />
+                    </svg>
+
+                    </span>
+
+                    <span class="absolute bottom-4 text-white z-20 font-medium">
+                        Vista previa de este curso
+                    </span>
+                </button>
+            </div>
+
+            <div class="p-6">
+                <div class="mb-6">
+                    <div class="mb-3 text-4xl font-semibold">
+                        89,99 €
+                    </div>
+                    <div class="mb-2 flex items-stretch justify-between gap-2">
+                        <a href="#" class="flex-1 py-3 text-center font-medium text-white bg-udemy-light hover:bg-udemy-dark">
+                            Añadir a la cesta
+                        </a>
+                        <button class="flex items-center border border-primary px-3 hover:bg-gray-200 hover:cursor-pointer">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                            </svg>
+                        </button>
+                    </div>
+                    <button class="mb-4 block w-full py-2 border border-primary text-center font-semibold">
+                        Comprar ahora
+                    </button>
+                    <div class="text-xs text-center">
+                        Garantía de reembolso de 30 días
+                    </div>
+                </div>
+
+                <div class="mb-4">
+                    <div class="my-2 font-semibold">
+                        Este curso incluye:
+                    </div>
+                    <ul class="text-sm space-y-2">
+                        <li class="flex items-center gap-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25" />
+                            </svg>
+
+                            <div>
+                                14,5 horas de vídeo bajo demanda
+                            </div>
+                        </li>
+                        <li class="flex items-center gap-3"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                            </svg>
+                            <div>
+                                2 artículos
+                            </div>
+                        </li>
+                        <li class="flex items-center gap-3"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M9 13.5l3 3m0 0l3-3m-3 3v-6m1.06-4.19l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
+                            </svg>
+                            <div>
+                                22 recursos descargables
+                            </div>
+                        </li>
+                        <li class="flex items-center gap-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
+                            </svg>
+                            <div>
+                                Acceso en dispositivos móviles y TV
+                            </div>
+                        </li>
+                        <li class="flex items-center gap-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+
+                            <div>
+                                Acceso de por vida
+                            </div>
+                        </li>
+                        <li class="flex items-center gap-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+                            </svg>
+                            <div>
+
+                                Subtítulos
+                            </div>
+                        </li>
+                        <li class="flex items-center gap-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z" />
+                            </svg>
+                            <div>
+                                Audiodescripción en un audio existente
+                            </div>
+                        </li>
+                        <li class="flex items-center gap-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 18.75h-9m9 0a3 3 0 013 3h-15a3 3 0 013-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 01-.982-3.172M9.497 14.25a7.454 7.454 0 00.981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 007.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 002.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 012.916.52 6.003 6.003 0 01-5.395 4.972m0 0a6.726 6.726 0 01-2.749 1.35m0 0a6.772 6.772 0 01-3.044 0" />
+                            </svg>
+                            <div>
+                                Certificado de finalización
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="mb-6 flex justify-center gap-16">
+                    <a href="#" class="text-gray-700 hover:text-gray-900 font-semibold underline decoration-udemy underline-offset-4 text-sm">
+                        Compartir
+                    </a>
+                    <a href="#" class="text-gray-700 hover:text-gray-900 font-semibold underline decoration-udemy underline-offset-4 text-sm">
+                        Regalar este curso
+                    </a>
+                </div>
+
+                <div class="text-center">
+                    <a href="#" class="text-gray-700 hover:text-gray-900 font-semibold underline decoration-udemy underline-offset-4 text-sm">
+                        Aplicar cupón
+                    </a>
+                </div>
+            </div>
+
+            <hr>
+
+            <div class="p-6">
+                <p class="text-xl font-semibold">
+                    ¿La formación es para 2 o más personas?
+                </p>
+                <p class="my-3 text-sm">
+                    Dale a tu equipo acceso a más de 25.000 de los mejores cursos de Udemy en cualquier momento y lugar.
+                </p>
+
+                <a href="#" class="block py-2 border border-primary text-center font-semibold text-sm hover:bg-gray-200">
+                    Prueba udemy business
+                </a>
+            </div>
+        </div>
     </div>
 </div>
